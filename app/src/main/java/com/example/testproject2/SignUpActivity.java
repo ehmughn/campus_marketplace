4c690b6a02adf5bb334fdb3d287a75f7e83effc8
Eman <emanhatesschool1234@gmail.com>
2024-12-16 11:41:00 +0800

    app name change

--- app/src/main/java/com/example/testproject2/SignUpActivity.java
+++ app/src/main/java/com/example/testproject2/SignUpActivity.java
@@ -1,9 +1,7 @@
 package com.example.testproject2;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "TestProject2"
+rootProject.name = "Campus Marketplace"
 include(":app")
  
