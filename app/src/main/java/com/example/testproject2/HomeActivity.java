a489d9fecf86b89fe079f750cb9d48dd86c4e17a
Eman <emanhatesschool1234@gmail.com>
2025-01-03 14:48:43 +0800

    added button from home to profile

--- app/src/main/java/com/example/testproject2/HomeActivity.java
+++ app/src/main/java/com/example/testproject2/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.testproject2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class HomeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void toProfile(View v) {
+        Intent intent = new Intent(HomeActivity.this, ProfileActivity.class);
+        startActivity(intent);
     }
 }
