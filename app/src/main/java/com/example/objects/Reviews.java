e0a74a3e13be48e0267acaf9780cb7e73eaee0df
Eman <emanhatesschool1234@gmail.com>
2025-02-06 14:13:05 +0800

    more temporary product images and profile pictures

--- app/src/main/java/com/example/objects/Reviews.java
+++ app/src/main/java/com/example/objects/Reviews.java
@@ -31,8 +31,8 @@ public class Reviews {
         this.stars = random1.nextInt(5) + 1;
         this.variation = "Chochlet";
         this.comment = "We're no strangers to love. You know the rules and so do I. A full commitment's what I'm thinkin' of. You wouldn't get this from any other guy. I just wanna tell you how I'm feeling. Gotta make you understand. Never gonna give you up. Never gonna let you down. Never gonna run around and desert you. Never gonna make you cry. Never gonna say goodbye. Never gonna tell a lie and hurt you.";
-        this.image1 = R.drawable.cookies;
-        this.image2 = R.drawable.burger;
+        this.image1 = R.drawable.temp_product_cookies;
+        this.image2 = R.drawable.temp_product_burger;
         this.helpful_likes = random2.nextInt(999) + 1;
     }
 
